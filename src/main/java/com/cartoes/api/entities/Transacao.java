476ee33c76d09f3d5873d298833c2800dcdc7351
wildfireY <luiz06lhap@gmail.com>
2020-08-21 20:17:57 -0300

    Repository Transacao

--- src/main/java/com/cartoes/api/entities/Transacao.java
+++ src/main/java/com/cartoes/api/entities/Transacao.java
@@ -13,6 +13,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -21,85 +23,101 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 @Table(name = "transacao")
 public class Transacao implements Serializable {
-        private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 
-        @Column(name = "data_Transacao", nullable = false)
-        private Date dataTransacao;
+	@Column(name = "data_Transacao", nullable = false)
+	private Date dataTransacao;
 
-        @Column(name = "cnpj", nullable = false, length = 14)
-        private String cnpj;
+	@Column(name = "cnpj", nullable = false, length = 14)
+	private String cnpj;
 
-        @Column(name = "valor", nullable = false)
-        private Double valor;
+	@Column(name = "valor", nullable = false)
+	private Double valor;
 
-        @Column(name = "qdt_Parcelas", nullable = false)
-        private int qdtParcelas;
+	@Column(name = "qdt_Parcelas", nullable = false)
+	private int qdtParcelas;
 
-        @Column(name = "juros", nullable = false)
-        private Double juros;
+	@Column(name = "juros", nullable = false)
+	private Double juros;
 
-        @JsonBackReference
-        @ManyToOne(fetch = FetchType.EAGER)
-        private Cartao cartao;
+	@JsonBackReference
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Cartao cartao;
 
-		public int getId() {
-			return id;
-		}
+	public int getId() {
+		return id;
+	}
 
-		public void setId(int id) {
-			this.id = id;
-		}
+	public void setId(int id) {
+		this.id = id;
+	}
 
-		public Date getDataTransacao() {
-			return dataTransacao;
-		}
+	public Date getDataTransacao() {
+		return dataTransacao;
+	}
 
-		public void setDataTransacao(Date dataTransacao) {
-			this.dataTransacao = dataTransacao;
-		}
+	public void setDataTransacao(Date dataTransacao) {
+		this.dataTransacao = dataTransacao;
+	}
 
-		public String getCnpj() {
-			return cnpj;
-		}
+	public String getCnpj() {
+		return cnpj;
+	}
 
-		public void setCnpj(String cnpj) {
-			this.cnpj = cnpj;
-		}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
 
-		public Double getValor() {
-			return valor;
-		}
+	public Double getValor() {
+		return valor;
+	}
 
-		public void setValor(Double valor) {
-			this.valor = valor;
-		}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
 
-		public int getQdtParcelas() {
-			return qdtParcelas;
-		}
+	public int getQdtParcelas() {
+		return qdtParcelas;
+	}
 
-		public void setQdtParcelas(int qdtParcelas) {
-			this.qdtParcelas = qdtParcelas;
-		}
+	public void setQdtParcelas(int qdtParcelas) {
+		this.qdtParcelas = qdtParcelas;
+	}
 
-		public Double getJuros() {
-			return juros;
-		}
+	public Double getJuros() {
+		return juros;
+	}
 
-		public void setJuros(Double juros) {
-			this.juros = juros;
-		}
+	public void setJuros(Double juros) {
+		this.juros = juros;
+	}
 
-		public Cartao getCartao() {
-			return cartao;
-		}
+	public Cartao getCartao() {
+		return cartao;
+	}
 
-		public void setCartao(Cartao cartao) {
-			this.cartao = cartao;
-		}
+	public void setCartao(Cartao cartao) {
+		this.cartao = cartao;
+	}
 
+	@PreUpdate
+	public void preUpdate() {
+		dataTransacao = new Date();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		dataTransacao = new Date();
+	}
+
+	@Override
+	public String toString() {
+		return "Transacao[" + "id=" + id + "," + "dataTransacao=" + dataTransacao + "," + "cnpj=" + cnpj + ","
+				+ "valor=" + valor + "," + "qdtParcelas=" + qdtParcelas + "," + "juros=" + juros + "," + "cartao="
+				+ cartao + "]";
+	}
 }
---
+++ src/main/java/com/cartoes/api/repositories/TransacaoRepository.java
@@ -0,0 +1,16 @@
+package com.cartoes.api.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cartoes.api.entities.Transacao;
+
+@Transactional(readOnly = true)
+public interface TransacaoRepository extends JpaRepository<Transacao, Integer> {
+    @Query("SELECT ta FROM Transacao ta WHERE ta.cartao.id = :cartaoId")
+    List<Transacao> findByClienteId(@Param("cartaoId") int cartaoId);
+}
