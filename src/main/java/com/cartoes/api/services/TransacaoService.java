ff4a33bfb24d7997f981a4300bea7e4014cbb92b
wildfireY <luiz06lhap@gmail.com>
2020-08-22 17:47:04 -0300

    TransacaoService v2

--- src/main/java/com/cartoes/api/services/TransacaoService.java
+++ src/main/java/com/cartoes/api/services/TransacaoService.java
@@ -7,34 +7,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-
+import org.springframework.stereotype.Service;
 import com.cartoes.api.entities.Transacao;
 import com.cartoes.api.repositories.TransacaoRepository;
 import com.cartoes.api.utils.ConsistenciaException;
 
-public class TransacaoService {
-	private static final Logger log = LoggerFactory.getLogger(TransacaoService.class);
-	
-	@Autowired
-	private TransacaoRepository transacaoRepository;
 
-	public Optional<List<Transacao>> listarTransacoes(int cartaoId) throws ConsistenciaException {
-        log.info("Service: buscando as transacoes do cartao: {}", cartaoId);
+@Service
+public class TransacaoService {
+    private static final Logger log = LoggerFactory.getLogger(TransacaoService.class);
+
+    @Autowired
+    private TransacaoRepository transacaoRepository;
+    public Optional<List<Transacao>> listarTransacoes(int cartaoId) throws ConsistenciaException {
+        log.info("Service: buscando as transações do cartão: {}", cartaoId);
         Optional<List<Transacao>> transacao = Optional.ofNullable(transacaoRepository.findByCartaoId(cartaoId));
         if (!transacao.isPresent()) {
-            log.info("Service: Nenhuma cartão com id: {} foi encontrado", cartaoId);
+            log.info("Service: Nenhuma transação com id: {} foi encontrado", cartaoId);
             throw new ConsistenciaException("Nenhuma transacao com o idCartao: {} foi encontrado", cartaoId);
         }
         return transacao;
     }
-	public Transacao salvar(Transacao transacao) throws ConsistenciaException {
-		log.info("Service: salvando o cartao: {}", transacao);
-		try {
-			return transacaoRepository.save(transacao);
-		} catch (DataIntegrityViolationException e) {
-			log.info("Erro na Service");
-			throw new ConsistenciaException("Error");
 
-		}
-	}
+    public Transacao salvar(Transacao transacao) throws ConsistenciaException {
+        log.info("Service: salvando o cartao: {}", transacao);
+        try {
+            return transacaoRepository.save(transacao);
+        } catch (DataIntegrityViolationException e) {
+            log.info("Service: Já existe um cartão de número {} cadastrado CORRGIR EXCESSAO");
+            throw new ConsistenciaException("Já existe um cartão de número {} cadastrado CORRIGIR EXCESSAO");
+
+        }
+    }
+
 }
